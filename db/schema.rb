76cdfcd830a68e52f3cf2209ea91a30956123f58
vrc <vidya@umn.edu>
2017-05-19 18:51:15 +0000

    User stories 2 and 3 sign up: completed sans mailer

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170504164906) do
+ActiveRecord::Schema.define(version: 20170519152836) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -24,6 +24,10 @@ ActiveRecord::Schema.define(version: 20170504164906) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
   end
