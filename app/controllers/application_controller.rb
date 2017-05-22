ac17c8e6df6f13231ca548f21cd9002b271cd1dc
vrc <vidya@umn.edu>
2017-05-22 00:47:05 +0000

    User story 5 roles: implement pundit via test functionality but not final app functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,24 @@
 class ApplicationController < ActionController::Base
 
+  include Pundit
+rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
-def after_sign_in_path_for(resource)
-  if resource.class == User
-    users_show_path
-  end 
-end
+  def after_sign_in_path_for(resource)
+    if resource.class == User
+      users_show_path
+    end 
+  end
+  
+  
+  private
+  
+  def user_not_authorized
+    flash[:alert] = "You need to be the original Wiki author to update the Wiki"
+    redirect_to(@wiki)
+  end
   
 end
