ef3c18e30801a9a0fc0de867e877f90d96f6bc2a
vrc <vidya@umn.edu>
2017-05-25 17:09:58 +0000

    User story 9 markdown: completed UPDATED Home page and Account page too

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -1,10 +1,25 @@
 class Wiki < ActiveRecord::Base
+
   belongs_to :user
 
   after_initialize :init
 
+  # before_save :assign_markdown_content, [:new, :create]
+
     def init
       self.private  ||= false  #will set the default value only if it's nil
     end
+
+  # class << self
+  #   def markdown
+  #     Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+  #   end
+  # end
+
+  # def assign_markdown_content
+  #   assign_attributes({
+  #     markdown_content: self.class.markdown.render(content)
+  #   })
+  # end
 
 end
