308cab5650cdfe3f5c30e7906d63098f97120c01
vrc <vidya@umn.edu>
2017-11-03 20:02:45 +0000

    Changed routes to use Devise and added SMTP info

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,20 +41,20 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
 config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
- config.action_mailer.delivery_method = :smtp
-config.action_mailer.perform_deliveries = true 
-config.action_mailer.raise_delivery_errors = true
-config.action_mailer.default :charset => "utf-8"
+#  config.action_mailer.delivery_method = :smtp
+# config.action_mailer.perform_deliveries = true 
+# config.action_mailer.raise_delivery_errors = true
+# config.action_mailer.default :charset => "utf-8"
 
   
-      ActionMailer::Base.smtp_settings = {
-      address:        'smtp.sendgrid.net',
-      port:           '2525',
-      authentication: :plain,
-      user_name:      ENV['SENDGRID_USERNAME'],
-      password:       ENV['SENDGRID_PASSWORD'],
-      domain:         'heroku.com',
-      enable_starttls_auto: true
-    }
+#       ActionMailer::Base.smtp_settings = {
+#       address:        'smtp.sendgrid.net',
+#       port:           '2525',
+#       authentication: :plain,
+#       user_name:      ENV['SENDGRID_USERNAME'],
+#       password:       ENV['SENDGRID_PASSWORD'],
+#       domain:         'heroku.com',
+#       enable_starttls_auto: true
+#     }
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,21 +13,22 @@ end
 get 'users/show' #'/users/:id', to: 'users#show'
 
 ## devise controllers for users
-devise_for :user, controllers: {  
-  # confirmations: 'users/confirmations',
-  passwords: 'users/passwords',
-  registrations: 'users/registrations',
-  sessions: 'users/sessions',
-  # unlocks: 'users/unlocks',
-}, skip: [:sessions]
+devise_for :users
+# , controllers: {  
+#   # confirmations: 'users/confirmations',
+#   passwords: 'users/passwords',
+#   registrations: 'users/registrations',
+#   sessions: 'users/sessions',
+#   # unlocks: 'users/unlocks',
+# }, skip: [:sessions]
 
 ## custom routes for users
-as :user do  
-  get 'login' => 'users/sessions#new', as: :new_user_session
-  post 'login' => 'users/sessions#create', as: :user_session
-  delete 'logout' => 'users/sessions#destroy', as: :destroy_user_session
-  get 'register' => 'users/registrations#new'
-end
+# as :user do  
+#   get 'login' => 'users/sessions#new', as: :new_user_session
+#   post 'login' => 'users/sessions#create', as: :user_session
+#   delete 'logout' => 'users/sessions#destroy', as: :destroy_user_session
+#   get 'register' => 'users/registrations#new'
+# end
 
   get 'welcome/index'
 
