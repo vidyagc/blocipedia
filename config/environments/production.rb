e01dc0cff7eb146e20c99f88094e84495310d672
vrc <vidya@umn.edu>
2017-11-30 00:56:55 +0000

    added mailer info to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
 config.assets.precompile << "*.css"
+
+    config.action_mailer.default_url_options = { host: 'https://bloc-project-vrc17.c9users.io/' }
+
 end
