9a0e2ae722de06a66a04b4d52ece1706818f5b4b
vrc <vidya@umn.edu>
2018-01-09 14:38:23 +0000

    precompile

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,7 @@ module Wikit
   class Application < Rails::Application
     config.secret_key_base = '<%= ENV["SECRET_KEY_BASE"] %>'
     config.autoload_paths  = %W(#{config.root}/lib)
+    config.serve_static_assets = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
