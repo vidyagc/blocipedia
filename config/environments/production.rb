03c2c33bb657b1322770d479047661c10c2be85b
vrc <vidya@umn.edu>
2018-01-09 14:14:09 +0000

    Updated production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.assets.precompile << "*.css"
+  # config.assets.precompile << "*.css"
 
   config.action_mailer.default_url_options = { host: 'https://vn-wikit.herokuapp.com' }
 
